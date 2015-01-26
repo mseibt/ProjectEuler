31b6ff7cdb87b80af263f8c1b143b1138f8c66e9
msslx <michael.seibt@tum.de>
2015-01-26 22:52:04 +0100

    added another solution

--- 018.py
+++ 018.py
@@ -22,13 +22,32 @@ a triangle containing one-hundred rows; it cannot be solved by brute
 force, and requires a clever method! ;o)
 """
 
+triangle = [[int(number) for number in line.split()]
+            for line in open("resources/018_triangle.txt")]
+
+
 # instead of searching the way with the maximum sum
 # just add up the higher elements from the bottom to the top
 # this works since we don't have to know the exact route through the triangle
 # which gives the maximum
-triangle = [[int(number) for number in line.split()]
-            for line in open("resources/018_triangle.txt")]
-for row in range(len(triangle) - 1, 0, -1):
-    for col in range(0, row):
-        triangle[row - 1][col] += max(triangle[row][col], triangle[row][col + 1])
-print(triangle[0][0])
+def e018a():
+    for row in range(len(triangle) - 1, 0, -1):
+        for col in range(0, row):
+            triangle[row - 1][col] += max(triangle[row][col], triangle[row][col + 1])
+    return triangle[0][0]
+
+
+# If you start from top to bottom you have to take the maximum of the last row
+# Also accessing indices is a bit harder
+def e018b():
+    for row in range(len(triangle)-1):
+        for col in range(len(triangle[row])):
+            if col == 0:
+                triangle[row+1][col] += triangle[row][col]
+            if col == len(triangle[row])-1:
+                triangle[row+1][col+1] += triangle[row][col]
+            else:
+                triangle[row+1][col+1] += max(triangle[row][col], triangle[row][col+1])
+    return max(triangle[-1])
+
+print(e018b())
